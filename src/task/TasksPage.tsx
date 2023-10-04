0857a943452dfff76c9619712c9845aefb206748
fixiple <djessy.engo47@gmail.com>
2023-10-04 02:23:47 +0200

    putting all in a single file + adding tasks work

--- src/task/TasksPage.tsx
+++ src/task/TasksPage.tsx
@@ -1,21 +1,74 @@
 import { MOCK_TASKS } from './mockTasks';
-import TaskList from './TaskList';
-import NewTask from './NewTask';
+import { ChangeEvent, ChangeEventHandler, SyntheticEvent, useRef, useState } from 'react';
+import { Task } from './Task';
+
+
+
+function dataIteration(list: Array<Task>){
+    const inputRef = useRef<any>([]);
+    const listData = Object.values(list);
+    const listDataIteration = listData.map((task) => 
+        <li key={task.id} className="task"> 
+            <input 
+                id={task.name}
+                type="checkbox"
+                ref={inputRef.current[task.id]}
+            /> <label htmlFor={task.name}>{task.name}</label>
+        </li>
+    )
+
+    return listDataIteration;
+}
 
 function TasksPage() {
-    let data=MOCK_TASKS
+    const [list, setList] = useState(MOCK_TASKS)
+
+    const [taskName, setTaskName] = useState("")
+    var [taskID, setTaskID] = useState(list[list.length - 1].id+1 || 0);
+
+    function handleSubmit(event: SyntheticEvent){
+
+        const task: Task = new Task(
+            {
+                id: taskID,
+                name: taskName,
+                isActive: false
+            }
+        );
+        setTaskID(() => taskID+1)
+        setList([...list, task])
+    }
     
+    
+    
+    const handleChange = (e : React.ChangeEvent<HTMLInputElement>): void  => {
+        //console.log(e.currentTarget.value);
+        var title: string = e.currentTarget.value.trim();
+        if(e.currentTarget.value.trim().length>2){
+            //console.log("input is not empty");
+            setTaskName(title);
+        } else {
+            console.log("input is empty")
+        }
+    }
+
+
     return (
         <>
             <h1>Tasks</h1>
             <div id="tasks-list">
-                <TaskList tasklist={data}></TaskList>
+                <ul style = {{ listStyleType : 'none'}}>
+                    {dataIteration(list)}
+                </ul>
             </div>
             <div id="new-task-container">
-                <NewTask currentList={data}></NewTask>
+                <label htmlFor="task">task:</label> 
+                <input type="text" onChange={handleChange}  id="task"/>
+                <input type="Button" defaultValue="Add Todo" onClick={handleSubmit}/>
             </div>
         </>
     )
+
 }
 
 export default TasksPage;
