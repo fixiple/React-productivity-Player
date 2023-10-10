e4e76461eb3e30df0ce66b8bcab6b4ef59addd94
fixiple <djessy.engo47@gmail.com>
2023-10-11 01:23:32 +0200

    modification works but doesn't update in view....

--- src/task/TasksPage.tsx
+++ src/task/TasksPage.tsx
@@ -1,10 +1,10 @@
-import { addingTaskToTasks, getTasks} from './mockTasks';
+import { addingTaskToTasks, getTasks, initLocalTasks, deleteTasks, modifyTask} from './mockTasks';
 import { ChangeEvent, useRef, useState } from 'react';
 import { Task } from './Task';
 
 
 
-console.log(getTasks)
+
 
 function dataIteration(list: Array<Task>){
     const inputRef = useRef<any>([]);
@@ -23,13 +23,19 @@ function dataIteration(list: Array<Task>){
 }
 
 function TasksPage() {
+    //deleteTasks();
+    //initLocalTasks();
+
     var tasks : Array<Task> = getTasks();
+
+    //console.log(...tasks);
     const [taskName, setTaskName] = useState("")
-    var [taskID, setTaskID] = useState(0);
+    var [taskID, setTaskID] = useState(tasks.length + 1||0);
 
     // event = SyntheticEvent
     function handleSubmit(){
-
+        setTaskID((id) => id + 1);
+        
         const task: Task = new Task(
             {
                 id: taskID,
@@ -37,20 +43,26 @@ function TasksPage() {
                 isActive: false
             }
         );
-        setTaskID(() => tasks[tasks.length - 1].id+1 || taskID+1)
 
+        // console.log("task number: "+tasks[tasks.length-1].id);
+        // console.log("task id:" + taskID);
         addingTaskToTasks(task);
     }
-    
-    
+
+    //TODO: Modification works but not update, Update doesn't update the local tasks shown in the view/front.....
+    function modify(){
+        modifyTask(6,"OH NO I ORIN Again!", true);
+        tasks=JSON.parse(localStorage.getItem("taskList")!);
+    }
     
     const handleChange = (e : ChangeEvent<HTMLInputElement>): void  => {
         //console.log(e.currentTarget.value);
         var title: string = e.currentTarget.value.trim();
-        if(e.currentTarget.value.trim().length>2){
-            //console.log("input is not empty");
-            setTaskName(title);
-        } 
+        setTaskName(title);
+        // if(e.currentTarget.value.trim().length>2){
+        //     //console.log("input is not empty");
+        //     setTaskName(title);
+        // } 
         //else {
         //     console.log("input is empty")
         // }
@@ -69,6 +81,7 @@ function TasksPage() {
                 <label htmlFor="task">task:</label> 
                 <input type="text" onChange={handleChange}  id="task"/>
                 <input type="Button" defaultValue="Add Todo" onClick={handleSubmit}/>
+                <input type="Button" defaultValue="modify" onClick={modify}/>
             </div>
         </>
     )
--- src/task/mockTasks.ts
+++ src/task/mockTasks.ts
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { Task } from './Task';
 
 export const MOCK_TASKS = [
@@ -12,44 +11,59 @@ export const MOCK_TASKS = [
     name: 'Wisozk Group',
     isActive: false,
 })]
-function dad(){
-    
-    // use of localStorage to save items in browser, return an Storage Object
-    useEffect(() => {
-        const tasksLocal = JSON.parse(localStorage.getItem('todos')!);
-        if (tasksLocal) {
-            setTasks(tasksLocal);
-        }
-    }, []);
-    
-    const [tasks, setTasks] = useState(Array<Task>);
-    
 
-}
-function initLocalTasks() : void{
+export function initLocalTasks() : void{
     if (!localStorage.getItem("taskList")){
         localStorage.setItem(
-            "taskList", JSON.stringify([])
+            "taskList", 
+            JSON.stringify(MOCK_TASKS)
         )
     }
+    //console.log(JSON.parse(localStorage.getItem("taskList")||"empty"))
 }
 
-//we are getting the items from localStorage
-function getLocalTasks() : void {
-    initLocalTasks;    
-    const tasksLocal: Array<Task> = JSON.parse(localStorage.getItem("taskList")! );
-    setTasks([...tasksLocal])
-}
+
 export function getTasks(): Array<Task>{
-    initLocalTasks;
-    const tasksLocal: Array<Task> = JSON.parse(localStorage.getItem("taskList")! );
+
+    const tasksLocal: Array<Task> = JSON.parse(localStorage.getItem("taskList")!);
     return tasksLocal
 }
 
+
 export function addingTaskToTasks(task : Task){
+    let tasks : Array<Task> = getTasks();
     tasks.push(task)
     localStorage.setItem(
         "taskList", JSON.stringify(tasks)
     )
+}
+
+//TODO: Modification works but not update, Update doesn't update the local tasks shown in the view/front.....
+export function modifyTask(index : number, newName : string ="null", isDone : boolean=false){
+    let json : Array<Task> = JSON.parse(localStorage.getItem("taskList")!);
+
+    json[index].name??=newName||json[index].name;
+    // json[index].isActive??=isDone||false;
+
+    for(let i = 0; i < json.length; i++) {
+        if(json[i].id === index) {
+            json[i].name=newName
+            json[i].isActive=isDone
+        }
+    }
+    localStorage.setItem(
+        "taskList", JSON.stringify(json)
+    )
+}
+
+
+export function readFromLocalStorage( obj: string) : void{
+
+    let objToRead : string|null = localStorage.getItem(obj) || null;
+    //console.log(objToRead)
+}
+
+export function deleteTasks(): void{
+    localStorage.removeItem("taskList");
 }
 
